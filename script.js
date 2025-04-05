56ab01b3f0685832b18d2413cdff25d07b2eac70
M1K259 <87774553+Mika259@users.noreply.github.com>
2025-04-06 01:54:11 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+/*setTimeout(()=> {
+            const mT = new bootstrap.Toast(".toast", {
+                autohide: false
+            });
+            mT.show();
+        }, 5000)
+        setTimeout(()=>{
+            const md = new bootstrap.Modal("#exampleModalToggle")
+            md.show()
+        },100)*/
+        (function(){
+        function gd() {
+            let test = window.confirm("Subscribe to support me :)")
+            if (test){
+                window.open("https://youtube.com/@mikayt259?si=-Rz9eulERoncIhj6","_blank")
+                setTimeout(()=>{
+                    alert("Go to download!")
+                    window.open("https://drive.google.com/file/d/1SfW5sU6koppNwwAeYGrC_fyAXdk3vzKw/view?usp=sharing","_blank")
+                },5000)
+            }
+            else{
+                alert("Sorry you can't download from me :(")
+            }
+        }
+        
+        function gd_211() {
+            let test = window.confirm("Subscribe to support me :)")
+            if (test){
+                window.open("https://youtube.com/@mikayt259?si=-Rz9eulERoncIhj6","_blank")
+                setTimeout(()=>{
+                    alert("Go to download!")
+                    window.open("https://drive.google.com/file/d/1MlMa5MRyFZLpQ9aaFPR2decVABupNMxZ/view?usp=sharing","_blank")
+                },5000)
+            }
+            else{
+                alert("Sorry you can't download from me :(")
+            }
+        }
+        
+        
+        function gd_old() {
+            let test = window.confirm("Subscribe to support me :)")
+            if (test){
+                window.open("https://youtube.com/@mikayt259?si=-Rz9eulERoncIhj6","_blank")
+                setTimeout(()=>{
+                    alert("Go to download!")
+                    window.open("https://drive.google.com/file/d/14-0xCXFpv8xzb0iWdjh4ds1gGeEbv8nQ/view?usp=sharing","_blank")
+                },5000)
+            }
+            else{
+                alert("Sorry you can't download from me :(")
+            }
+        }
+
+
+        function dp() {
+            window.confirm("Are you sure want to download from Dropbox ?")
+        };})();
